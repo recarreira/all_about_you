ee762d593eb5e4277b5849127add6b59784f68f4
Renata Carreira <re.carreira@gmail.com>
2013-05-25 14:39:18 -0300

    Adding social_link helper

---
+++ _plugins/social_link.rb
@@ -0,0 +1,16 @@
+module Jekyll
+  module SocialLinkFilter
+    def social_link(input)
+      service, username = input
+
+      if service == "twitter"
+        link = "http://twitter.com/#{username}"
+      elsif service == "github"
+        link = "https://github.com/#{username}/"
+      end
+      "<a href='#{link}' title='#{service}'>#{service}</a>"
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::SocialLinkFilter)
