d6cc320594c0bac14e3a662686fb6207842101a2
Roberto Soares <roberto.tech@gmail.com>
2013-05-25 20:41:02 -0300

    social link: adding support for Facebook, flickr, foursquare, googleplus, lastfm, linked in, quora, stackoverflow

--- _plugins/social_link.rb
+++ _plugins/social_link.rb
@@ -1,16 +1,25 @@
 module Jekyll
   module SocialLinkFilter
+
+    SOCIAL_URLS = {
+      "facebook"      => "https://www.facebook.com/%s",
+      "flickr"        => "http://www.flickr.com/photos/%s/",
+      "foursquare"    => "https://foursquare.com/%s",
+      "github"        => "https://github.com/%s/",
+      "googleplus"    => "https://plus.google.com/%s?rel=author",
+      "lastfm"        => "http://www.lastfm.com.br/user/%s",
+      "linkedin"      => "http://www.linkedin.com/in/%s",
+      "quora"         => "http://www.quora.com/%s",
+      "stackoverflow" => "http://stackoverflow.com/users/%s/",
+      "twitter"       => "http://twitter.com/%s"
+    }
+
     def social_link(input)
       service, username = input
+      return "" if username.nil? || username.to_s.empty?
 
-      return "" if username.nil?
-
-      if service == "twitter"
-        link = "http://twitter.com/#{username}"
-      elsif service == "github"
-        link = "https://github.com/#{username}/"
-      end
-      "<a href='#{link}' class='fc-webicon #{service}' title='#{service}'>#{service}</a>"
+      url = SOCIAL_URLS[service] % username
+      "<a href='#{url}' class='fc-webicon #{service}' title='#{service}'>#{service}</a>"
     end
     def custom_link(input)
       title = input["title"]
