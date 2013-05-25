920274a01bb2cdeecfad9534c5bda26030599431
Roberto Soares <roberto.tech@gmail.com>
2013-05-25 14:58:15 -0300

    compass setup

--- _plugins/jekyll_asset_pipeline.rb
+++ _plugins/jekyll_asset_pipeline.rb
@@ -1,15 +1,25 @@
 require 'jekyll_asset_pipeline'
 
 module JekyllAssetPipeline
-  class SassConverter < JekyllAssetPipeline::Converter
-    require 'sass'
+  class CompassConverter < JekyllAssetPipeline::Converter
+    require 'compass'
+    require 'tempfile'
 
     def self.filetype
       '.sass'
     end
 
+    def initialize(asset)
+      @asset = asset
+      super
+    end
+
     def convert
-      return Sass::Engine.new(@content, syntax: :sass).render
+      output = Tempfile.new('compass_output')
+      Compass.add_project_configuration({})
+      Compass.configure_sass_plugin!
+      Compass.compiler.compile("./_assets/stylesheets/#{@asset.filename}", output.path)
+      output.read
     end
   end
 end
