3be287a79a4715188602bba4a3790e655ef2a924
Roberto Soares <roberto.tech@gmail.com>
2013-05-26 02:14:13 -0300

    adding asset_tag_template to fix css path

--- _plugins/jekyll_asset_pipeline.rb
+++ _plugins/jekyll_asset_pipeline.rb
@@ -24,3 +24,20 @@ module JekyllAssetPipeline
     end
   end
 end
+
+
+module JekyllAssetPipeline
+  class CssTagTemplate < JekyllAssetPipeline::Template
+    def self.filetype
+      '.css'
+    end
+
+    def self.priority
+      -1
+    end
+
+    def html
+      "<link href='#{@path}/#{@filename}' rel='stylesheet' type='text/css' />\n"
+    end
+  end
+end
